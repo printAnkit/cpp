1b3c26a82d0552b4954550ecd3ac54474cc168ab
Ankit Gaur <89451736+printAnkit@users.noreply.github.com>
2022-12-31 17:50:25 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+
+    for(i=0;i<6;i++){
+        cout<<endl;
+        for(j=0;j<6;j++){
+            cout<<"* ";
+        }
+    }
+}
